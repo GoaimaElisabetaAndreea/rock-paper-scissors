93192ad2803137dd13f9e37f56b035c590b44179
GoaimaElisabetaAndreea <elisabetagoaima@gmail.com>
2024-10-11 20:42:44 +0300

    added an user interface

--- game/script.js
+++ game/script.js
@@ -1,92 +1,74 @@
-function getUserInput()
-{
-    let humanChoice
-    let ok = false;
-    while(ok == false)
-    {
-        humanChoice = prompt("What are you picking? Rock, paper or scissors?").toLowerCase();
-        if(!(humanChoice == "rock" || humanChoice == "paper" || humanChoice == "scissors"))
-            {
-                console.log("Sorry, your choice is wrong. Please input rock, paper or scissors");
-            }
-            else
-            {
-                ok = true;
-            }
-    } 
-    return humanChoice;
+let scoreHuman = 0;
+let scoreComputer = 0;
+let roundNumber = 1;
+
+function getUserInput() {
+    const selectElement = document.querySelector('.choice');
+    return selectElement.value;
 }
 
-function getComputerChoice()
-{
+function getComputerChoice() {
     let randomInt = Math.floor(Math.random() * 3) + 1;
-    if(randomInt == 1)
-    {
+    if (randomInt == 1) {
         return "rock";
-    }
-    else if(randomInt == 2)
-    {
+    } else if (randomInt == 2) {
         return "paper";
-    }
-    else
-    {
+    } else {
         return "scissors";
     }
 }
 
-function getWinner(humanChoice, computerChoice)
-{
+function getWinner(humanChoice, computerChoice) {
     const outcomes = {
-        "rock": {"rock": "Tie", "paper":"Computer wins", "scissors": "Human wins"},
+        "rock": {"rock": "Tie", "paper": "Computer wins", "scissors": "Human wins"},
         "paper": {"rock": "Human wins", "paper": "Tie", "scissors": "Computer wins"},
-        "scissors": {"rock": "Computer Wins", "paper": "Human Wins", "scissors": "Tie"}
+        "scissors": {"rock": "Computer wins", "paper": "Human wins", "scissors": "Tie"}
     };
 
     return outcomes[humanChoice][computerChoice];
 }
 
-function getWinnerBasedOnScore(scoreHuman, scoreComputer)
-{
-    if(scoreHuman > scoreComputer)
-    {
-        return "You won with a score of " + scoreHuman + " at " + scoreComputer;
-    }
-    else if(scoreHuman < scoreComputer)
-    {
-        return "You lose with a score of " + scoreComputer + " at " + scoreHuman;
-    }
-    else 
-    {
+function getWinnerBasedOnScore(scoreHuman, scoreComputer) {
+    if (scoreHuman > scoreComputer) {
+        return "You won with a score of " + scoreHuman + " to " + scoreComputer;
+    } else if (scoreHuman < scoreComputer) {
+        return "You lose with a score of " + scoreComputer + " to " + scoreHuman;
+    } else {
         return "It is a tie. Score: " + scoreHuman + "-" + scoreComputer;
     }
 }
 
-function play()
-{
-    let i = 0;
-    let scoreComputer = 0;
-    let scoreHuman = 0;
-    while(i != 5)
-    {
-        let humanChoice = getUserInput();
-        let computerChoice = getComputerChoice();
-        let winner = getWinner(humanChoice, computerChoice);
-        if(winner === "Tie")
-        {
-            console.log("It's a tie !");
-        }
-        else if(winner === "Human wins")
-        {
-            console.log("Congrats, you won! " + humanChoice + " beats " + computerChoice);
+function playRound() {
+    if (roundNumber <= 3) {
+        const humanChoice = getUserInput();
+        const computerChoice = getComputerChoice();
+        const winner = getWinner(humanChoice, computerChoice);
+        let paragraph = document.querySelector('.result');
+        
+        if (winner === "Tie") {
+            paragraph.textContent = "Round " + roundNumber + ": It's a tie! You both chose " + humanChoice;
+        } else if (winner === "You win") {
+            paragraph.textContent = "Round " + roundNumber + ": You won! " + humanChoice + " beats " + computerChoice;
             scoreHuman++;
-        }
-        else{
-            console.log("You losed, computer won. " + humanChoice + " beats " + computerChoice);
+        } else {
+            paragraph.textContent = "Round " + roundNumber + ": You lost! " + computerChoice + " beats " + humanChoice;
             scoreComputer++;
         }
-        i++;
+
+        roundNumber++;
+        
+        if (roundNumber > 3) {
+            paragraph.textContent += " Final Result: " + getWinnerBasedOnScore(scoreHuman, scoreComputer);
+            resetGame(); 
+        }
     }
-    console.log(getWinnerBasedOnScore(scoreHuman, scoreComputer))
 }
 
-play();
+function resetGame() {
+    scoreHuman = 0;
+    scoreComputer = 0;
+    roundNumber = 1;
+}
+
+const playButton = document.querySelector('.play');
+playButton.addEventListener("click", playRound);
